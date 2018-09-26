fbad74e0a05e23dd585503b9de862293f92082f4
Omar Eid <omar_eid21@yahoo.com>
2018-09-26 12:56:19 -0500

    Commented unused aliases

--- aliases.sh
+++ aliases.sh
@@ -34,14 +34,14 @@ function cd(){
 alias cdworkspace=$(DIRS_WORKSPACE)
 alias cdworkspace=$(DIRS_WORKSPACE)
 
-# Custom File Editors
-alias editcontainers="vi ${DIRS_ENVIRONMENT}/commands/containers.sh"
-alias containers="cat ${DIRS_ENVIRONMENT}/commands/containers.sh"
-alias editenvs="vi ${DIRS_ENVIRONMENT}/envmgr/config.json"
-alias envs="cat ${DIRS_ENVIRONMENT}/envmgr/config.json"
-alias editbookmarks="vi ${DIRS_ENVIRONMENT}/bookmarks.sh"
-alias bookmarks="cat ${DIRS_ENVIRONMENT}/bookmarks.sh"
-alias editaliases="vi ${DIRS_ENVIRONMENT}/aliases.sh"
+# Custom File Editors - Didnt end up using these ...
+# alias editcontainers="vi ${DIRS_ENVIRONMENT}/commands/containers.sh"
+# alias containers="cat ${DIRS_ENVIRONMENT}/commands/containers.sh"
+# alias editenvs="vi ${DIRS_ENVIRONMENT}/envmgr/config.json"
+# alias envs="cat ${DIRS_ENVIRONMENT}/envmgr/config.json"
+# alias editbookmarks="vi ${DIRS_ENVIRONMENT}/bookmarks.sh"
+# alias bookmarks="cat ${DIRS_ENVIRONMENT}/bookmarks.sh"
+# alias editaliases="vi ${DIRS_ENVIRONMENT}/aliases.sh"
 
 # Ease directory traversal. Shouldnt need to go up more than 5 dirs
 alias .1='push ../'
