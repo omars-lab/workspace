3d22b1b571bf9cd6903fb4605b023e0c900af290
Omar Eid <omar_eid21@yahoo.com>
2019-10-31 09:16:48 -0500

    Added setup script and getting compinit to work on share comp

---
+++ setup.sh
@@ -0,0 +1,8 @@
+
+# Install Plugins ...
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/antigen ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/antigen
+
+# Install iterm tools ...
+# curl -L https://iterm2.com/shell_integration/install_shell_integration_and_utilities.sh | bash
+# mv /Users/omareid/.iterm2_shell_integration.zsh ~/Workspace/binaries/iterm2_shell_integration.zsh
