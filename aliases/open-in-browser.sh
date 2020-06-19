0ce563886d65ce397fd21d4d159753239ea8c7a6
Omar Eid <omar_eid21@yahoo.com>
2020-06-18 20:37:31 -0500

    Cleaining up opening aliases

--- aliases/open-in-browser.sh
+++ aliases/open-in-browser.sh
@@ -15,43 +15,51 @@ function bitbucket_url() {
 function open:chrome-tab() {
   open -na "Google Chrome" --args --new-tab "${1}"
 }
+DEFAULT_BROWSER=open:chrome-tab
+alias browser="${DEFAULT_BROWSER}"
 
-function open:chrome-tab-with-git-repo() {
+function open:git-repo-in-browser-tab() {
   (git config --get remote.origin.url | grep github) &&
-    open:chrome-tab-with-github-repo || open:chrome-tab-with-bitbucket-repo
+    open:github-repo-in-browser-tab || open:bitbucket-repo-in-browser-tab
 }
+alias git-repo='open:git-repo-in-browser-tab'
 
-function open:chrome-tab-with-github-repo() {
+function open:github-repo-in-browser-tab() {
   local REPO_URL=$(github_url)
   echo "Opening Github Repo: ${REPO_URL}"
-  open:chrome-tab "${REPO_URL}"
+  ${BROWSER:-${DEFAULT_BROWSER}} "${REPO_URL}"
 }
+alias github-repo='open:github-repo-in-browser-tab'
 
-function open:chrome-tab-with-bitbucket-repo() {
+function open:bitbucket-repo-in-browser-tab() {
   local REPO_URL=$(bitbucket_url)
   echo "Opening Bitbucket Repo: ${REPO_URL}"
-  open:chrome-tab "${REPO_URL}"
+  ${BROWSER:-${DEFAULT_BROWSER}} "${REPO_URL}"
 }
+alias bitbucket-repo='open:bitbucket-repo-in-browser-tab'
 
-function open:chrome-tab-with-file-in-git-repo() {
+function open:git-file-in-browser-tab() {
   local FILE="${1}"
   (git config --get remote.origin.url | grep github) \
-    && ( open:chrome-tab-with-file-in-github-repo "${FILE}" ) \
-    || ( open:chrome-tab-with-file-in-bitbucket-repo "${FILE}" )
+    && ( open:github-file-in-browser-tab "${FILE}" ) \
+    || ( open:bitbucket-file-in-browser-tab "${FILE}" )
 }
+alias git-file='open:git-file-in-browser-tab'
 
-function open:chrome-tab-with-file-in-github-repo() {
+function open:github-file-in-browser-tab() {
   local RELATIVE_FILE=$(git ls-files --full-name "${1}")
   local REPO_URL=$(github_url "blob")
   echo "Opening File ${RELATIVE_FILE} Github Repo: ${REPO_URL}"
   local URL=${REPO_URL}/${RELATIVE_FILE}
-  open:chrome-tab "${URL}"
+  ${BROWSER:-${DEFAULT_BROWSER}} "${URL}"
 }
+alias github-file='open:github-file-in-browser-tab'
 
-function open:chrome-tab-with-file-in-bitbucket-repo() {
+function open:bitbucket-file-in-browser-tab() {
   local RELATIVE_FILE=$(git ls-files --full-name "${1}")
   local REPO_URL=$(bitbucket_url)
   echo "Opening File ${RELATIVE_FILE} BitBucket Repo: ${REPO_URL}"
   local URL=${REPO_URL}/${RELATIVE_FILE}
-  open:chrome-tab "${URL}"
+  ${BROWSER:-${DEFAULT_BROWSER}} "${URL}"
 }
+alias bitbucket-file='open:bitbucket-file-in-browser-tab'
