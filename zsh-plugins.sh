6fd7976b7949b7b4fb85fa1928deebbfe4ad9821
Omar Eid <omar_eid21@yahoo.com>
2020-07-21 10:12:47 -0500

    Cleaning up conda init

---
+++ zsh-plugins.sh
@@ -0,0 +1,2 @@
+ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
+test -d ${ZSH_CUSTOM}/plugins/zsh-completions || git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM}/plugins/zsh-completions
