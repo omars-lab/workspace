890e7e0be119e36d13744f675c126a4d775c2a97
Omar Eid <omar_eid21@yahoo.com>
2022-04-23 17:20:19 -0500

    Updating installer script to log stuff

--- installers/sharedworkspace-installer.sh
+++ installers/sharedworkspace-installer.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+INSTALL_PREFIX="${1}"
+INSTALL_LOGS=${INSTALL_PREFIX}/install.logs
+
+date >> ${INSTALL_LOGS}
+whoami >> ${INSTALL_LOGS}
+pwd >> ${INSTALL_LOGS}
+env >> ${INSTALL_LOGS}
+
 # Inject into ZSH Profile 
 if ( test -f ${HOME}/.zshrc && ! ( grep -q '#tag:inject-brew-loader' ${HOME}/.zshrc) )
 then 
     echo 'source $(first $(brew --prefix sharedspace)/loader-brew.sh ${CURRENT_DIR}/loader-brew.sh) #tag:inject-brew-loader' >> ${${HOME}/.zshrc}
+    echo "Injected brew loader into zshrc" >> ${INSTALL_LOGS}
 fi
 
 # Inject into BASH Profile 
 if ( test -f ${HOME}/.bash_profile && ! ( grep -q '#tag:inject-brew-loader' ${HOME}/.bash_profile) )
 then 
     echo 'source $(first $(brew --prefix sharedspace)/loader-brew.sh ${CURRENT_DIR}/loader-brew.sh) #tag:inject-brew-loader' >> ${${HOME}/.bash_profile}
+    echo "Injected brew loader into bash_profile" >> ${INSTALL_LOGS}
 fi
